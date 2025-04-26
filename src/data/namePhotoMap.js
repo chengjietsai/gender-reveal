f576bcbb90bf4bba55d082931fce87bca2aef32f
CJ Tsai <chengjie1993@hotmail.com>
2025-04-26 21:48:48 +0800

    fix path

--- src/data/namePhotoMap.js
+++ src/data/namePhotoMap.js
@@ -1,6 +1,7 @@
+import lulu from '../assets/avatars/lulu.jpg'
 export const namePhotoMap = {
     // "lulu": "https://raw.githubusercontent.com/chengjietsai/gender-reveal/friend-specific/src/assets/avatars/lulu.png",
-    "lulu":"./src/assets/avatars/lulu.jpg"
+    "lulu": lulu,
     // "Bob": "https://raw.githubusercontent.com/chengjietsai/gender-reveal/friend-specific/src/assets/avatars/bob.jpg",
     // "Charlie": "https://raw.githubusercontent.com/chengjietsai/gender-reveal/friend-specific/src/assets/avatars/charlie.jpg",
     // "Diana": "https://raw.githubusercontent.com/chengjietsai/gender-reveal/friend-specific/src/assets/avatars/diana.jpg",
